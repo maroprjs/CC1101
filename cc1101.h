8a443aacfdbb70d0a85775281cf5e181e3a55e22
MARO <maroprjs>
2017-09-12 18:59:15 +0200

    header file settings from Steffen Hille's research 7/2017

--- cc1101.h
+++ cc1101.h
@@ -207,19 +207,19 @@ enum RFSTATE
  * or when the radio enters RXFIFO_OVERFLOW state. In TX the pin will de-assert if the TX FIFO underflows
  * Settings optimized for low current consumption
  */
-//#define CC1101_DEFVAL_IOCFG2     0x29        // GDO2 Output Pin Configuration
-#define CC1101_DEFVAL_IOCFG2     0x2E        // GDO2 Output Pin Configuration
+//#define CC1101_DEFVAL_IOCFG2   0x29        // GDO2 Output Pin Configuration
+#define CC1101_DEFVAL_IOCFG2     0x0D        // GDO2 Output Pin Configuration
 #define CC1101_DEFVAL_IOCFG1     0x2E        // GDO1 Output Pin Configuration
-#define CC1101_DEFVAL_IOCFG0     0x06        // GDO0 Output Pin Configuration
-#define CC1101_DEFVAL_FIFOTHR    0x07        // RX FIFO and TX FIFO Thresholds
-#define CC1101_DEFVAL_SYNC1      0xB5        // Synchronization word, high byte
-#define CC1101_DEFVAL_SYNC0      0x47        // Synchronization word, low byte
+#define CC1101_DEFVAL_IOCFG0     0x2D        // GDO0 Output Pin Configuration
+#define CC1101_DEFVAL_FIFOTHR    0x47        // RX FIFO and TX FIFO Thresholds
+#define CC1101_DEFVAL_SYNC1      0xD3        // Synchronization word, high byte
+#define CC1101_DEFVAL_SYNC0      0x91        // Synchronization word, low byte
 #define CC1101_DEFVAL_PKTLEN     0x3D        // Packet Length
-#define CC1101_DEFVAL_PKTCTRL1   0x06        // Packet Automation Control
-#define CC1101_DEFVAL_PKTCTRL0   0x05        // Packet Automation Control
+#define CC1101_DEFVAL_PKTCTRL1   0x04        // Packet Automation Control
+#define CC1101_DEFVAL_PKTCTRL0   0x32        // Packet Automation Control
 #define CC1101_DEFVAL_ADDR       0xFF        // Device Address
 #define CC1101_DEFVAL_CHANNR     0x00        // Channel Number
-#define CC1101_DEFVAL_FSCTRL1    0x08        // Frequency Synthesizer Control
+#define CC1101_DEFVAL_FSCTRL1    0x06        // Frequency Synthesizer Control
 #define CC1101_DEFVAL_FSCTRL0    0x00        // Frequency Synthesizer Control
 // Carrier frequency = 868 MHz
 #define CC1101_DEFVAL_FREQ2_868  0x21        // Frequency Control Word, High Byte
@@ -236,29 +236,29 @@ enum RFSTATE
 
 // Carrier frequency = 433 MHz
 #define CC1101_DEFVAL_FREQ2_433  0x10        // Frequency Control Word, High Byte
-#define CC1101_DEFVAL_FREQ1_433  0xA7        // Frequency Control Word, Middle Byte
-#define CC1101_DEFVAL_FREQ0_433  0x62        // Frequency Control Word, Low Byte
+#define CC1101_DEFVAL_FREQ1_433  0xB0        // Frequency Control Word, Middle Byte
+#define CC1101_DEFVAL_FREQ0_433  0xB0       // Frequency Control Word, Low Byte
 
-#define CC1101_DEFVAL_MDMCFG4    0xCA        // Modem Configuration
-#define CC1101_DEFVAL_MDMCFG3    0x83        // Modem Configuration
-#define CC1101_DEFVAL_MDMCFG2    0x93        // Modem Configuration
+#define CC1101_DEFVAL_MDMCFG4    0x69     // Modem Configuration
+#define CC1101_DEFVAL_MDMCFG3    0xE4      // Modem Configuration
+#define CC1101_DEFVAL_MDMCFG2    0x30        // Modem Configuration
 #define CC1101_DEFVAL_MDMCFG1    0x22        // Modem Configuration
 #define CC1101_DEFVAL_MDMCFG0    0xF8        // Modem Configuration
-#define CC1101_DEFVAL_DEVIATN    0x35        // Modem Deviation Setting
+#define CC1101_DEFVAL_DEVIATN    0x00      // Modem Deviation Setting
 #define CC1101_DEFVAL_MCSM2      0x07        // Main Radio Control State Machine Configuration
 //#define CC1101_DEFVAL_MCSM1      0x30        // Main Radio Control State Machine Configuration
-#define CC1101_DEFVAL_MCSM1      0x20        // Main Radio Control State Machine Configuration
+#define CC1101_DEFVAL_MCSM1      0x30        // Main Radio Control State Machine Configuration
 #define CC1101_DEFVAL_MCSM0      0x18        // Main Radio Control State Machine Configuration
-#define CC1101_DEFVAL_FOCCFG     0x16        // Frequency Offset Compensation Configuration
+#define CC1101_DEFVAL_FOCCFG     0x14        // Frequency Offset Compensation Configuration
 #define CC1101_DEFVAL_BSCFG      0x6C        // Bit Synchronization Configuration
-#define CC1101_DEFVAL_AGCCTRL2   0x43        // AGC Control
-#define CC1101_DEFVAL_AGCCTRL1   0x40        // AGC Control
-#define CC1101_DEFVAL_AGCCTRL0   0x91        // AGC Control
+#define CC1101_DEFVAL_AGCCTRL2   0x04        // AGC Control
+#define CC1101_DEFVAL_AGCCTRL1   0x00       // AGC Control
+#define CC1101_DEFVAL_AGCCTRL0   0x92        // AGC Control
 #define CC1101_DEFVAL_WOREVT1    0x87        // High Byte Event0 Timeout
 #define CC1101_DEFVAL_WOREVT0    0x6B        // Low Byte Event0 Timeout
-#define CC1101_DEFVAL_WORCTRL    0xFB        // Wake On Radio Control
-#define CC1101_DEFVAL_FREND1     0x56        // Front End RX Configuration
-#define CC1101_DEFVAL_FREND0     0x10        // Front End TX Configuration
+#define CC1101_DEFVAL_WORCTRL    0xF8        // Wake On Radio Control
+#define CC1101_DEFVAL_FREND1     0xB6        // Front End RX Configuration
+#define CC1101_DEFVAL_FREND0     0x11        // Front End TX Configuration
 #define CC1101_DEFVAL_FSCAL3     0xE9        // Frequency Synthesizer Calibration
 #define CC1101_DEFVAL_FSCAL2     0x2A        // Frequency Synthesizer Calibration
 #define CC1101_DEFVAL_FSCAL1     0x00        // Frequency Synthesizer Calibration
